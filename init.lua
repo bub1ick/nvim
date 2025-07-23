8054727f3ba576d61168bd8e0a40d5707c84e7c7
bub1ick <smirnovkostya@inbox.ru>
2025-07-24 02:47:12 +0300

    Removed lazy.nvim plugin manager. Replaced by vim "packages" feature. Added git submodules for plugins as a package.

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- Visuals
-vim.o.guifont = "ProggyVector:h14:w0"
+--vim.o.guifont = "ProggyVector:h14:w0"
 vim.cmd.highlight({ "Comment", "gui=italic" })
 vim.opt.linespace = 0
 
@@ -99,33 +99,7 @@ vim.opt.completeopt = { "menu", "menuone", "popup", "noinsert" }
 -- All custom mappings
 require("mappings")
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable",
-    lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
-end
+vim.cmd.colorscheme("moonfly")
 
-vim.opt.rtp:prepend(lazypath)
--- Lazy plugin manager
-local lazy = require("lazy")
-lazy.setup("lazyspec")
-
-vim.cmd.colorscheme("flexoki-dark")
-
--- lspconfig
-local lspconfig = require("lspconfig")
-lspconfig.clangd.setup({
-    cmd_env = { CPATH = vim.env.INCLUDE }
-}) -- C/C++ config
-lspconfig.cmake.setup({}) -- CMake config
+--vim.lsp.config("clangd")
+vim.lsp.enable("clangd")
